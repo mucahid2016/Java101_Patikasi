aa802667b2097c5e44ef079d76ea8ad963fed578
mucahid2016 <81246450+mucahid2016@users.noreply.github.com>
2022-03-01 12:44:52 +0300

    Java 101 Patikası

--- src/Degiskenler/Taksimetre_Hesaplama_4/Main.java
+++ src/Degiskenler/Taksimetre_Hesaplama_4/Main.java
@@ -4,13 +4,13 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        double gidilecekMesafe, acilisUcreti = 10, kmBasiUcret = 2.20, toplamUcret, toplamTutar;
+        double gidilecekMesafe, acilisUcreti = 10, kmBasiUcret = 2.20, tutar, toplamTutar;
         Scanner scan = new Scanner(System.in);
         System.out.print("Gidilecek mesafe : ");
         gidilecekMesafe = scan.nextDouble();
 
-        toplamUcret = acilisUcreti + (gidilecekMesafe * kmBasiUcret);
-        toplamTutar = (toplamUcret <= 20) ? 20 : toplamUcret;
+        tutar = acilisUcreti + (gidilecekMesafe * kmBasiUcret);
+        toplamTutar = (tutar <= 20) ? 20 : tutar;
         System.out.println( "Toplam Tutar : "+toplamTutar);
     }
 }
