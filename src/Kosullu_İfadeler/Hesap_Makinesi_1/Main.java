8e6bbc7bbf38215b1ae745946ccd4e19b4052fe2
mucahid2016 <81246450+mucahid2016@users.noreply.github.com>
2022-03-04 09:44:34 +0300

    Java 101 Patikası

--- src/Kosullu_İfadeler/Hesap_Makinesi_1/Main.java
+++ src/Kosullu_İfadeler/Hesap_Makinesi_1/Main.java
@@ -15,22 +15,42 @@ public class Main {
         System.out.println("1-Toplama\n2-Çıkarma\n3-Çarpma\n4-Bölme");
         System.out.print("Seçiminiz : ");
         select = sc.nextInt();
-
-        if (select == 1) {
-            System.out.println("Toplam : " + (n1 + n2));
-        } else if (select == 2) {
-            System.out.println("Çıkarma : " + (n1 - n2));
-        } else if (select == 3) {
-            System.out.println("Çarpma : " + (n1 * n2));
-        } else if (select == 4) {
-            if (n2!=0){
-                System.out.println("Bölme : " + (n1 / n2));
-            }else {
-                System.out.println("Bir sayı 0'a bölünemez !!!");
-            }
-
-        }else{
-            System.out.println("Hatalı Seçim Yaptınız. Tekrar Deneyiniz !");
+        //İf-Else ile hesap makinesi oluşturma
+//        if (select == 1) {
+//            System.out.println("Toplam : " + (n1 + n2));
+//        } else if (select == 2) {
+//            System.out.println("Çıkarma : " + (n1 - n2));
+//        } else if (select == 3) {
+//            System.out.println("Çarpma : " + (n1 * n2));
+//        } else if (select == 4) {
+//            if (n2!=0){
+//                System.out.println("Bölme : " + (n1 / n2));
+//            }else {
+//                System.out.println("Bir sayı 0'a bölünemez !!!");
+//            }
+//
+//        }else{
+//            System.out.println("Hatalı Seçim Yaptınız. Tekrar Deneyiniz !");
+//        }
+        // Switch-Case ile hesap makinesi yapımı
+        switch (select){
+            case 1:
+                System.out.println("Toplam : "+(n1+n2));
+                break;
+            case 2:
+                System.out.println("Çıkarma : "+(n1-n2));
+                break;
+            case 3:
+                System.out.println("Çarpma : "+(n1*n2));
+                break;
+            case 4:
+                if (n2!=0){
+                    System.out.println("Bölme : "+(n1/n2));
+                }else {
+                    System.out.println("Bir sayı 0'a bölünemez !");
+                }
+                break;
+            default:
         }
 
     }
