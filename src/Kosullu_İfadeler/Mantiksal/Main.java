bc158a301cbd747addd051f0cf18f72f82956cb3
mucahid2016 <81246450+mucahid2016@users.noreply.github.com>
2022-03-04 09:21:31 +0300

    Java 101 Patikası

--- src/Kosullu_İfadeler/Mantiksal/Main.java
+++ src/Kosullu_İfadeler/Mantiksal/Main.java
@@ -3,6 +3,32 @@ package Kosullu_İfadeler.Mantiksal;
 
 public class Main {
     public static void main(String[] args)  {
+        int day=1;
+        switch (day){
+            case 1:
+                System.out.println("Bugün Pazartesi");
+                break;
+            case 2:
+                System.out.println("Bugün SAlı");
+                break;
+            case 3:
+                System.out.println("Bugün Çarşamba");
+                break;
+            case 4:
+                System.out.println("Bugün Çarşamba");
+                break;
+            case 5:
+                System.out.println("Bugün Çarşamba");
+                break;
+            case 6:
+                System.out.println("Bugün Çarşamba");
+                break;
+            case 7:
+                System.out.println("Bugün Çarşamba");
+                break;
+            default:
+                System.out.println("Hatalı Giriş Yaptınız !");
+        }
 
     }
 }
