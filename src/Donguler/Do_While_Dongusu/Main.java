aff1e24a6212c27ef29b5d4b19ff1a1304a6bc69
mucahid2016 <81246450+mucahid2016@users.noreply.github.com>
2022-03-07 22:47:24 +0300

    Java 101 Patikası

---
+++ src/Donguler/Do_While_Dongusu/Main.java
@@ -0,0 +1,11 @@
+package Donguler.Do_While_Dongusu;
+
+public class Main {
+    public static void main(String[] args) {
+        int year=2021;
+        do {
+            System.out.println(year);
+            year++;
+        }while (year<2021);
+    }
+}
