1c2870fbe26c94b7966622f4dac845cc378c59f5
mucahid2016 <81246450+mucahid2016@users.noreply.github.com>
2022-03-07 22:37:12 +0300

    Java 101 Patikası

--- src/Donguler/While_Dongusu/Main.java
+++ src/Donguler/While_Dongusu/Main.java
@@ -1,19 +1,41 @@
 package Donguler.While_Dongusu;
 
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        int i=1,k;
-        System.out.println("Program Başladı.");
-        while (i<=5){
-            System.out.println(i);
-            i++;
-            k=1;
-            while (k<=10){
-                System.out.print(k+"-");
-                k++;
-            }
-            System.out.println();
+
+        int password;
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Şifre Giriniz : ");
+        password=sc.nextInt();
+
+        if (password==123){
+            System.out.println("Doğru");
+        }else {
+            System.out.println("Yanlış");
         }
-        System.out.println("Program Bitti.");
+
+
+
+//        int left=100, right=200;
+//        while (++left<--right);
+//        System.out.println("100 ve 200'ün ortak Noktası : "+left);
+
+        //////////
+
+//        int i=1,k;
+//        System.out.println("Program Başladı.");
+//        while (i<=5){
+//            System.out.println(i);
+//            i++;
+//            k=1;
+//            while (k<=10){
+//                System.out.print(k+"-");
+//                k++;
+//            }
+//            System.out.println();
+//        }
+//        System.out.println("Program Bitti.");
     }
 }
