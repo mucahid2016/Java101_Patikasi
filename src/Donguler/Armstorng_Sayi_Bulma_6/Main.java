0919f8190849ef771120eab1e90d9cb49e885c86
mucahid2016 <81246450+mucahid2016@users.noreply.github.com>
2022-03-09 14:39:11 +0300

    Java 101 Patikası

--- src/Donguler/Armstorng_Sayi_Bulma_6/Main.java
+++ src/Donguler/Armstorng_Sayi_Bulma_6/Main.java
@@ -1,7 +1,68 @@
 package Donguler.Armstorng_Sayi_Bulma_6;
 
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
+        int a, sum = 0;
+        Scanner input = new Scanner(System.in);
+        System.out.print("Basamak Değerlerini toplamak istediğiniz sayıyı giriniz : ");
+        a = input.nextInt();
+
+        while (a != 0) {
+            int rakam = a % 10;
+            sum += rakam;
+            a /= 10;
+        }
+        System.out.println("Rakamlarım tolamı: "+sum);
+
+        //////////////////
+//        System.out.print("Sayı Giriniz : ");
+//        int number = input.nextInt();
+//        int basNumber = 0;
+//        int tempNumber = number;
+//        int basValue;
+//        int result = 0;
+//        int basPow;
+//
+//        while (tempNumber != 0) {
+//            tempNumber /= 10;
+//            basNumber++;
+//        }
+//
+//        tempNumber = number;
+//        while (tempNumber != 0) {
+//            basValue = tempNumber % 10;
+//            basPow = 1;
+//            for (int i = 1; i <= basNumber; i++) {
+//                basPow *= basValue;
+//            }
+//            result += basPow;
+//            tempNumber /= 10;
+//        }
+//
+//        if (result == number) {
+//            System.out.println(number + " sayısı ARMSTRONG sayıdır.");
+//        } else {
+//            System.out.println(number + " sayısı ARMSTRONG sayı değildir.");
+//        }
+
+//        int a = 2451, basamakSAyisi = 0, numberCounter = 0;
+//
+//        //Basamak Sayısı Bulma İşlemi
+//        //2451 /10 =245
+//        //245 / 10 = 24
+//        //24 / 10 = 2
+//        // 2 / 10 = 0
+//
+//        while (a != 0) {
+//            numberCounter++;
+//            a = a / 10;
+//        }
+//
+//        // Bir Sayının Son Basamağını Bulma
+//        //2451 % 10 = 1
+
 
     }
 }
