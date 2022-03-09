b06666acf6083903249a0e9507cb7a7d17648727
mucahid2016 <81246450+mucahid2016@users.noreply.github.com>
2022-03-09 15:39:44 +0300

    Java 101 Patikası

---
+++ src/Donguler/Yildizlarla_Ucgen_Yapma_8/Main.java
@@ -0,0 +1,31 @@
+package Donguler.Yildizlarla_Ucgen_Yapma_8;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.print("Basamak Sayısını Giriniz : ");
+        int n = input.nextInt();
+
+        for (int i = 1; i <= n; i++) {
+            for (int k = 1; k <= (n - i); k++) {
+                System.out.print(" ");
+            }
+            for (int j = 1; j <= (2 * i - 1); j++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+
+        for (int x = n - 1; x > 0; x--) {
+            for (int y = 1; y <= n - x; y++) {
+                System.out.print(" ");
+            }
+            for (int z = 1; z <= 2 * x - 1 ; z++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+}
