629540c0123d7ca47841c05f0c214ef835d7a9a4
mucahid2016 <81246450+mucahid2016@users.noreply.github.com>
2022-03-12 10:20:48 +0300

    Java 101 Patikası

--- src/Metotlar/Konular/Recursive_Metotlar/Main.java
+++ src/Metotlar/Konular/Recursive_Metotlar/Main.java
@@ -4,14 +4,18 @@ public class Main {
 
 
     static int f(int n) {
+        System.out.println("n => " + n);
         if (n == 1) {
             return 1;
         }
-        return f(n - 1) + n;
+        int result = f(n - 1) + n;
+        System.out.println("result >= " + result);
+        return result;
+
     }
 
     public static void main(String[] args) {
-        System.out.println(f(3));
+        System.out.println(f(4));
 
     }
 }
