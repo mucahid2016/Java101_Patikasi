18e35f78bcf175b328da4d1ec10680817cb56bc9
mucahid2016 <81246450+mucahid2016@users.noreply.github.com>
2022-03-12 09:03:57 +0300

    Java 101 Patikası

--- src/Metotlar/Konular/Return_Void/Main.java
+++ src/Metotlar/Konular/Return_Void/Main.java
@@ -1,12 +1,22 @@
 package Metotlar.Konular.Return_Void;
 
 public class Main {
-    static int sum(int a, int b){
-
-        return a+b;
+    static int sum(int a, int b) {
+        int result=a+b;
+        System.out.println(result);
+        return result;
     }
-    public static void main(String[] args) {
 
+    static void sum2(int a, int b){
+        int result=a+b;
+        System.out.println(result);
+    }
+
+    public static void main(String[] args) {
+        int result = sum(5, 2)+sum(3,10);
+        System.out.println(result);
+        System.out.println("******++++****");
+        sum2(5,99);
 
     }
 }
