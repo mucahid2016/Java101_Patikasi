cef10fcf8a4da27979d863712097334095314892
mucahid2016 <81246450+mucahid2016@users.noreply.github.com>
2022-03-19 12:49:09 +0300

    Java 101 Patikası

---
+++ src/Metotlar/Uslu_Sayi_4/Main.java
@@ -0,0 +1,29 @@
+package Metotlar.Uslu_Sayi_4;
+
+import java.util.Scanner;
+
+public class Main {
+    static int power(int n, int k) {
+        int result = 1;
+        //1 3 9 27 81
+        //n^0=1
+        //n^1=n
+        //n^2=n.n
+        //n^3=n.n.n
+        if (k == 0) {
+            return 1;
+        }
+        result = power(n, k - 1) * n;
+        return result;
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n, k;
+        System.out.print("Taban değeri giriniz : ");
+        n = sc.nextInt();
+        System.out.print("Üs Değerini Giriniz : ");
+        k = sc.nextInt();
+        System.out.println(power(n, k));
+    }
+}
