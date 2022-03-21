89f3dc98752851b2931e5acca441b3ce362df087
mucahid2016 <81246450+mucahid2016@users.noreply.github.com>
2022-03-21 10:16:52 +0300

    Java 101 Patikası

--- src/Siniflar/Tanim/Car.java
+++ src/Siniflar/Tanim/Car.java
@@ -1,25 +1,44 @@
 package Siniflar.Tanim;
 
 public class Car {
+    //Nitelikler
     String type = "sedan";
     String model;
     String color;
     int speed;
-    int speedLimit = 180;
+    int speedLimit;
+
+    Car(String model, int speed, String color) {
+        this.model = model;
+        this.speed = speed;
+        this.color = color;
+        this.type = "Sedan";
+        this.speedLimit = 180;
+    }
+    Car(){
+        System.out.println("Boş Kurucu Metodu Oluştu !");
+    }
 
     void increaseSpeed(int increment) {
-        if (speed + increment < speedLimit) {
-            speed += increment;
-        }
+        this.speed += increment;
+
     }
 
     void decreaseSpeed(int decrease) {
-        if (speed > 0) {
-            speed -= decrease;
+        if (this.speed > 0) {
+            this.speed -= decrease;
         }
     }
 
     void printSpeed() {
-        System.out.println(model + " Hızınız : " + speed);
+        System.out.println(this.model + " Hızınız : " + this.speed);
+    }
+
+    void printInfo() {
+        System.out.println("=============");
+        System.out.println("Model :\t"+this.model);
+        System.out.println("Color :\t"+this.color);
+        System.out.println("Type :\t"+this.type);
+        System.out.println("Speed :\t"+this.speed);
     }
 }
--- src/Siniflar/Tanim/Main.java
+++ src/Siniflar/Tanim/Main.java
@@ -2,25 +2,27 @@ package Siniflar.Tanim;
 
 public class Main {
     public static void main(String[] args) {
-        Car audi = new Car();
-        audi.model = "Audi A7";
-        audi.type = "Sports";
-        audi.speed = 10;
-        audi.increaseSpeed(20);
-        audi.decreaseSpeed(10);
-        audi.printSpeed();
+        Car audi=new Car("Audi",10,"blue");
+        audi.printInfo();
+
+        Car bmw=new Car("BMW",120,"yellow");
+        bmw.printInfo();
 
 
-        Car bmw = new Car();
-        bmw.model = "BMW";
-        bmw.speed = 20;
-        bmw.increaseSpeed(100);
-        bmw.increaseSpeed(100);
-        bmw.printSpeed();
+//        Car audi = new Car();
+//        audi.model = "Audi A7";
+//        audi.type = "Sports";
+//        audi.speed = 10;
 
-        Car mercedes = new Car();
-        mercedes.model = "Mercedes";
-        mercedes.speed = 30;
-        mercedes.printSpeed();
+
+//        Car bmw = new Car();
+//        bmw.model = "BMW";
+//        bmw.speed = 20;
+//
+//;
+//        Car mercedes = new Car();
+//        mercedes.model = "Mercedes";
+//        mercedes.speed = 30;
+
     }
 }
