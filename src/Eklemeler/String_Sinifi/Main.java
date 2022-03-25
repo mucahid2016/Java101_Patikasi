bec20aade286e441cdb9ca4d182c73728a09bc0a
mucahid2016 <81246450+mucahid2016@users.noreply.github.com>
2022-03-25 22:57:30 +0300

    Java 101 Patikası

---
+++ src/Eklemeler/String_Sinifi/Main.java
@@ -0,0 +1,11 @@
+package Eklemeler.String_Sinifi;
+
+public class Main {
+    public static void main(String[] args) {
+        String str = "patika";
+        String patika = "paTika";
+        System.out.println(str.codePointAt(2));
+        System.out.println(patika.codePointAt(2));
+        System.out.println(patika);
+    }
+}
